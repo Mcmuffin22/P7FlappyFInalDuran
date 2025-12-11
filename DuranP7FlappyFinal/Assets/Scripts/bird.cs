1ee2428454b6a81a945ec44e651cb01da9193fc9
EddyArch <760325@student.dusd.net>
2025-12-11 15:28:42 -0800

    finished coding movement
    did coding started aniumm madting am on 4th video

--- DuranP7FlappyFinal/Assets/Scripts/bird.cs
+++ DuranP7FlappyFinal/Assets/Scripts/bird.cs
@@ -3,25 +3,39 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class bird : MonoBehaviour{
-    
+
+    public float upforce = 200f;
+
     private bool isDead = false;
-    
+    private Rigidbody2D rb2d;
+    Animator anim;
+
     // Start is called before the first frame update
-    void Start() {
-        
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
         if (isDead == false)
-        { 
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                rb2d.velocity = Vector2.zero;
+                rb2d.AddForce(new Vector2(0, upforce));
+                anim.SetTrigger("FLap");
+            }
+        }
+    }
 
-
-
-
-
-
-        } 
-    }       
+    private void OnCollisionEnter2D()
+    {
+        isDead = true;
+        anim.SetTrigger("die");
+        //GameController.instance.BirdDieed();
+    }
 }
+  
