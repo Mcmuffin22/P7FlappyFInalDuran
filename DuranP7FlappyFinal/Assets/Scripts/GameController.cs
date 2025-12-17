0d7ad36cd70696d2ba1d26c451fe8c09cc52e733
EddyArch <760325@student.dusd.net>
2025-12-17 12:41:41 -0800

    submitted my game
    completd mt flappy bird game and submitted

---
+++ DuranP7FlappyFinal/Assets/Scripts/GameController.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameControl : MonoBehaviour {
+
+    public static GameControl instance;
+    public GameObject gameOverText;
+    public bool gameover = false;
+    public float scrollSpeed = -1.5f;
+    private bool gameOver;
+
+    // Use this for initialization
+    void Awake  () 
+    { 
+       if (instance == null ) {
+            instance = this;
+        } else if (instance != this)
+        {
+            Destroy (gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gameOver == true && Input.GetMouseButtonDown(0))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+
+    public void BirdDied()
+    {
+        gameOverText.SetActive(true);
+        gameover = true;
+    }
+}
--- DuranP7FlappyFinal/Assets/Scripts/bird.cs
+++ DuranP7FlappyFinal/Assets/Scripts/bird.cs
@@ -2,13 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class bird : MonoBehaviour{
+public class Bird : MonoBehaviour{
 
     public float upforce = 200f;
 
     private bool isDead = false;
     private Rigidbody2D rb2d;
-    Animator anim;
+    private Animator anim;
 
     // Start is called before the first frame update
     void Start()
@@ -26,7 +26,7 @@ public class bird : MonoBehaviour{
             {
                 rb2d.velocity = Vector2.zero;
                 rb2d.AddForce(new Vector2(0, upforce));
-                anim.SetTrigger("FLap");
+                anim.SetTrigger("Flap");
             }
         }
     }
@@ -34,8 +34,8 @@ public class bird : MonoBehaviour{
     private void OnCollisionEnter2D()
     {
         isDead = true;
-        anim.SetTrigger("die");
-        //GameController.instance.BirdDieed();
+        anim.SetTrigger("Die");
+        GameControl.instance.BirdDied();
     }
 }
   
